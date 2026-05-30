be326b417bbdc5dab6325d8615dd916ace49bcc4
ShizukuNya <131782131+ShizukuNya@users.noreply.github.com>
2026-05-30 16:28:43 +0800

    new post & comment enable

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -3,17 +3,17 @@ import { SITE_LANG } from "./siteConfig";
 
 // 评论系统配置
 export const commentConfig: CommentConfig = {
-  enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
-  system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
+  enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+  system: "giscus", // 评论系统选择: "twikoo" | "giscus"
   twikoo: {
     envId: "https://twikoo.vercel.app",
     lang: SITE_LANG,
   },
   giscus: {
     repo: "ShizukuNya/shizukunya.github.io",
-    repoId: "your-repo-id",
+    repoId: "R_kgDOSUihxg",
     category: "Announcements",
-    categoryId: "your-category-id",
+    categoryId: "DIC_kwDOSUihxs4C9OOa",
     mapping: "pathname",
     strict: "0",
     reactionsEnabled: "1",
