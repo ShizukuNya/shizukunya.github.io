33afceb1b84bac0773463c026b74adc6a123a6e3
ShizukuNya <131782131+ShizukuNya@users.noreply.github.com>
2026-05-30 16:56:04 +0800

    日常打理

--- src/config/randomPostsConfig.ts
+++ src/config/randomPostsConfig.ts
@@ -2,6 +2,6 @@ import type { RandomPostsConfig } from "../types/config";
 
 // 随机文章配置
 export const randomPostsConfig: RandomPostsConfig = {
-	enable: true,
-	maxCount: 5,
+  enable: false,
+  maxCount: 5,
 };
